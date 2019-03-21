39365d70a214a2f7ab214798ff0d4a86bbceafdc
Chris Tram <chris@mytime.com>
2019-03-21 00:21:12 -0700

    adds postgres packages and hooksup sequel

---
+++ db/index.js
@@ -0,0 +1,9 @@
+const Sequelize = require('sequelize');
+
+// Option 1: Passing parameters separately
+const sequelize = new Sequelize('database', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'postgres'
+});
+
+module.exports = sequelize;
