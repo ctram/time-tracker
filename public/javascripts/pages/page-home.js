5268df7c9fe1dd3574e0c33cfa9c9cb8c539b40a
Chris Tram <ctram@protonmail.com>
2019-03-21 19:35:33 -0700

    Create page-home.js
    adds placeholder home page

---
+++ public/javascripts/pages/page-home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class PageHome extends React.Component {
+    render() {
+        return (
+            <div className="d-flex justify-content-center">
+                This is home page
+            </div>
+        );
+    }
+}
