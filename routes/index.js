ea4c43a3c4928a8f187df2764de3f375d03679b2
Chris Tram <ctram@protonmail.com>
2019-03-21 19:32:41 -0700

    redirect all routes to root
    redirect all routes to root

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-//   res.redirect('index.html');
+router.get('/*', function(req, res, next) {
+  res.redirect('/');
 });
 
 module.exports = router;
