06a0cad90fdd96dd086f7b7a2d0a37c08eadd3e7
Chris Tram <ctram@protonmail.com>
2019-03-21 19:35:52 -0700

    stubs page login
    stubs page login

--- public/javascripts/pages/page-login.js
+++ public/javascripts/pages/page-login.js
@@ -2,6 +2,16 @@ import React from 'react';
 import { LoginForm } from '../components/login-form';
 
 export class PageLogin extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { email: null, password: null };
+    }
+
+    login() {
+
+    }
+
     render() {
         return (
             <div className="d-flex justify-content-center">
