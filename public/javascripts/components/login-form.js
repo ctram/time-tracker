8d5259818b1850a47d9c7f729c32cf1c78bd1e6b
Chris Tram <chris@mytime.com>
2019-03-21 21:17:08 -0700

    Update login-form.js
    format

--- public/javascripts/components/login-form.js
+++ public/javascripts/components/login-form.js
@@ -20,17 +20,36 @@ export class LoginForm extends React.Component {
     }
 
     render() {
-        return <form>
-            <div className="form-group">
-                <label htmlFor="exampleInputEmail1">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required />
-                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-            </div>
-            <div className="form-group">
-                <label htmlFor="exampleInputPassword1">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" required />
-            </div>
-            <button onClick={this.login} type="submit" className="btn btn-primary">Submit</button>
-        </form>;
+        return (
+            <form>
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Email address</label>
+                    <input
+                        type="email"
+                        className="form-control"
+                        id="exampleInputEmail1"
+                        aria-describedby="emailHelp"
+                        placeholder="Enter email"
+                        required
+                    />
+                    <small id="emailHelp" className="form-text text-muted">
+                        We'll never share your email with anyone else.
+                    </small>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="exampleInputPassword1">Password</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        id="exampleInputPassword1"
+                        placeholder="Password"
+                        required
+                    />
+                </div>
+                <button onClick={this.login} type="submit" className="btn btn-primary">
+                    Submit
+                </button>
+            </form>
+        );
     }
 }
