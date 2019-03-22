f2a9c8c3ef7099b38f06701d050bddc5fbbfb7b7
Chris Tram <chris@mytime.com>
2019-03-21 22:49:28 -0700

    Update index.js
    adds route for post

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/*', function(req, res, next) {
+router.get(/.*/, function(req, res, next) {
+    console.log('GET GET GET GET');
+    res.redirect('/');
+});
+
+router.post(/.*/, function(req, res, next) {
+    console.log('POST POST POST POST');
     res.redirect('/');
 });
 
