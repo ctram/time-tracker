935a1835ab78eb26765e8633bee1c16c01015cf8
Chris Tram <chris@mytime.com>
2019-03-23 22:10:29 -0700

    Update users.js
    removes get users, just use post users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', function (req, res, next) {
+    console.log('post users /post users /post users /post users /post users /post users /');
+    res.send('respond with a resource');
 });
 
 module.exports = router;
