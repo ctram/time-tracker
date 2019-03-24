0d1b36bf1a636e0872ae39f33261a93af53aef8b
Chris Tram <chris@mytime.com>
2019-03-23 22:11:13 -0700

    Create page-sign-up.js
    adds page component for sign up

---
+++ public/javascripts/pages/page-sign-up.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { LoginForm } from '../components/login-form';
+import { urls } from '../constants/urls';
+
+export class PageSignUp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { email: null, password: null };
+    }
+
+    signUp({ email, password }) {
+        fetch('http://localhost:3000/users', {
+            method: 'POST',
+            body: JSON.stringify({ email, password })
+        }).then(res => {
+            debugger;
+        });
+    }
+
+    render() {
+        return (
+            <div className="d-flex flex-column align-items-center">
+                <h3>Sign Up</h3>
+                <LoginForm handleSubmission={this.signUp} />
+            </div>
+        );
+    }
+}
