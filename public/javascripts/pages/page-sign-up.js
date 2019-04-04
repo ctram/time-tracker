d0ddee2e3ae16813d2cb1fd8bf2e58c99c7a19f6
Chris Tram <chris@mytime.com>
2019-04-03 23:35:01 -0700

    Update page-sign-up.js
    WIP redirect to login page after successful creation of user

--- public/javascripts/pages/page-sign-up.js
+++ public/javascripts/pages/page-sign-up.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import { LoginForm } from '../components/login-form';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { PageLogin } from './page-login';
+
 import { urls } from '../constants/urls';
 
 export class PageSignUp extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { email: null, password: null };
+
+        this.state = { email: null, password: null, redirectToLogin: false };
+
+        this.signUp = this.signUp.bind(this);
+        this.redirectToLogin = this.redirectToLogin.bind(this);
+    }
+
+    redirectToLogin() {
+        console.log('redirectologin called');
+        this.context.history.push('/login');
+
+        // TODO: refer to this to figure out how to redirect https://stackoverflow.com/questions/42701129/how-to-push-to-history-in-react-router-v4
+
+        // <Router>
+        //     <Route path="/login" component={PageLogin} />
+        // </Router>;
     }
 
     signUp({ email, password }) {
+        const _this = this;
+
         fetch('http://localhost:3000/users', {
             method: 'POST',
             headers: new Headers({
@@ -16,7 +36,9 @@ export class PageSignUp extends React.Component {
             }),
             body: JSON.stringify({ email: email, password: password })
         }).then(res => {
-            debugger;
+            if (res.status === 201) {
+                _this.redirectToLogin();
+            }
         });
     }
 
