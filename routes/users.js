cec79b3c755967dc7c595788b06793dfee0082d6
Chris Tram <chris@mytime.com>
2019-04-03 22:26:29 -0700

    Update users.js
    add user to database

--- routes/users.js
+++ routes/users.js
@@ -2,23 +2,17 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
-router.post('/users', function (req, res, next) {
-  const { email, password } = req.body;
-
-  console.log('route: /users');
-
-  console.log(User);
-  
-  User.build({ email, password }).then(user => {
-    console.log('newly built user ${user}');
-  })
-
-  console.log(`users request ${email + ' ' + password}`);
-
 router.post('/', function(req, res, next) {
     const { email, password } = req.body;
-    console.log(req.body);
-    res.send('respond with a resource');
+
+    User.create({ email, password })
+        .then(user => {
+            throw 'forced error';
+            res.json({ user });
+        })
+        .catch(err => {
+            next({ message: err });
+        });
 });
 
 module.exports = router;
