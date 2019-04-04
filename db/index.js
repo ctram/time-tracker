36d0c256cecb1a7d0baad47045d7fe804223be41
Chris Tram <chris@mytime.com>
2019-04-03 22:25:28 -0700

    Update index.js
    connect with correct credentials

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 // Option 1: Passing parameters separately
-const sequelize = new Sequelize('database', 'username', 'password', {
+const sequelize = new Sequelize('time_tracker_development', '', '', {
     host: 'localhost',
     dialect: 'postgres'
 });
