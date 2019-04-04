c588f66ed15e11921c1984c73fcd091bc7435fb9
Chris Tram <chris@mytime.com>
2019-04-03 22:24:52 -0700

    Update app.js
    asdasd

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ const logger = require('morgan');
 
 const sequelize = require('./db/index');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
