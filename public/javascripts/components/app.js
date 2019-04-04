8f0be1f34425481f344af82c2c1496d669178c1d
Chris Tram <chris@mytime.com>
2019-04-03 23:24:43 -0700

    Update app.js
    remove unused object

--- public/javascripts/components/app.js
+++ public/javascripts/components/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Navbar } from './navbar';
 import { PageLogin } from '../pages/page-login';
 import { PageHome } from '../pages/page-home';
