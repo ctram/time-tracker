0c509fa094990841570b591f5512640f2457e356
Chris Tram <ctram@protonmail.com>
2019-04-03 17:37:10 -0700

    formatting
    formatting

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get(/.*/, function(req, res, next) {
+router.get(/.*/, function (req, res, next) {
     console.log('GET GET GET GET');
     res.redirect('/');
 });
 
-router.post(/.*/, function(req, res, next) {
+router.post(/.*/, function (req, res, next) {
     console.log('POST POST POST POST');
     res.redirect('/');
 });
