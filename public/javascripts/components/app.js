8dd34d874d47d278c5f5130c6c63e32311411261
Chris Tram <ctram@protonmail.com>
2019-04-10 17:13:51 -0700

    Update app.js
    inject history into react router

--- public/javascripts/components/app.js
+++ public/javascripts/components/app.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import { Navbar } from './navbar';
 import { PageLogin } from '../pages/page-login';
-import { PageHome } from '../pages/page-home';
 import { PageSignUp } from '../pages/page-sign-up';
+import { PageHome } from '../pages/page-home';
+import history from '../browser-history';
 
 export class App extends React.Component {
     render() {
         return (
-            <Router>
+            <Router history={history}>
                 <div>
                     <Navbar />
                 </div>
                 <div>
                     <Route path="/login" component={PageLogin} />
                     <Route path="/sign-up" component={PageSignUp} />
-                    {/*
-                        <Route path="/home" exact component={PageHome} />
-                    */}
+                    <Route path="/home" exact component={PageHome} />
                 </div>
             </Router>
         );
