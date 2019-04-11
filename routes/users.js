36820d620bb2b869c048f03f2ad5925e7c469fff
Chris Tram <ctram@protonmail.com>
2019-04-10 17:19:01 -0700

    Update users.js
    formatting

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
     const { email, password } = req.body;
 
     User.create({ email, password })
