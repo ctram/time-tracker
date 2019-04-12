9e8ae3eda8b5cfb2032fc0c77bc8693841ef2672
Chris Tram <ctram@protonmail.com>
2019-04-11 17:57:59 -0700

    adds passport
    adds passport

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy;
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const loginRouter = require('./routes/login');
+
 const sequelize = require('./db/index');
+
+const User = require('./models/user');
 
 sequelize
     .authenticate()
@@ -14,10 +23,6 @@ sequelize
     .catch(err => {
         console.error('Unable to connect to the database:', err);
     });
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const loginRouter = require('./routes/login');
 
 const app = express();
 
@@ -30,6 +35,38 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(
+    {
+        usernameField: 'email'
+    },
+    (username, password, done) => {
+        const email = username;
+
+        User.findOne({ where: { email } })
+            .then(user => {
+
+
+                if (!user) {
+                    return done(null, false, { message: 'Incorrect username.' });
+                }
+
+                if (user.password !== password) {
+                    return done(null, false, { message: 'Incorrect password.' });
+                }
+
+                return done(null, user);
+            })
+            .catch(err => {
+
+
+                return done(err);
+            });
+    }
+));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -44,7 +81,7 @@ app.use(function (req, res, next) {
 // error handler
 app.use(function (err, req, res, next) {
     let error = err.error || err;
-    
+
     // set locals, only providing error in development
     res.locals.message = error.message;
     res.locals.error = req.app.get('env') === 'development' ? error : {};
--- routes/login.js
+++ routes/login.js
@@ -1,23 +1,11 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
-const User = require('../models/user');
 
-router.post('/', function (req, res, next) {
-    const { email, password } = req.body;
-
-    User.findOne({ where: { email } })
-        .then(user => {
-            if (user) {
-                if (user.password === password) {
-                    return res.status(200).json({ user });
-                }
-            }
-
-            return Promise.reject({ type: 'IncorrectCredentials', status: 401 });
-        })
-        .catch(error => {
-            next({ error })
-        })
-});
+router.post('/',
+    passport.authenticate('local'),
+    (req, res, next) => {
+        // TODO: here
+    });
 
 module.exports = router;
