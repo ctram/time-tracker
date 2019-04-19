00b7e349749db67a029b841571768f19ba36ef7b
Chris Tram <chris@mytime.com>
2019-04-18 23:09:42 -0700

    adds provider to app

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './components/app.js';
+import { Provider } from 'react-redux';
+import configureStore from './configureStore';
 
-ReactDOM.render(<App />, document.getElementById('app-root'))
+const store = configureStore({ currentUser: null });
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('app-root')
+);
