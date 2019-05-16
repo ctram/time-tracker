6e7073e243986467cf919e05bb20f14d546829bf
Chris Tram <chris@mytime.com>
2019-05-15 21:47:18 -0700

    remove unused code

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './components/app.js';
 import { Provider } from 'react-redux';
-import configureStore from './configureStore';
-
-const store = configureStore({ currentUser: null });
+import { store } from './configureStore.js';
 
 ReactDOM.render(
     <Provider store={store}>
