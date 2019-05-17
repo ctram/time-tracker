2ef61fdcd00c2662f67d8ee3bd8a3e08b9238b59
Chris Tram <chris@mytime.com>
2019-05-16 22:46:11 -0700

    adds file for tracker ui

---
+++ public/javascripts/components/tracker.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+// import { connect } from 'react-redux';
+
+export class TrackerComponent extends React.Component {
+    render() {
+        // TODO: here
+    }
+}
+
+// const mapStateToProps = state => {
+//     return {
+//         currentUser: state.users.currentUser
+//     };
+// };
+
+// const Navbar = connect(mapStateToProps)(NavbarComponent);
+
+// export { Navbar };
