27a1422991411374d87e9c7d7c38a493a19a8e0e
Chris Tram <chris@mytime.com>
2019-05-21 22:15:03 -0700

    grab currentUser state to App component, to decide router logic

--- public/javascripts/components/app.js
+++ public/javascripts/components/app.js
@@ -2,16 +2,21 @@ import React from 'react';
 
 import { Router, Route } from 'react-router-dom';
 
+import { connect } from 'react-redux';
+
 import { Navbar } from './navbar';
 
 import { PageLogin } from '../pages/page-login';
 import { PageSignUp } from '../pages/page-sign-up';
 import { PageHome } from '../pages/page-home';
+import { PageTimeLog } from '../pages/page-time-log';
 
 import history from '../browser-history';
 
-export class App extends React.Component {
+export class AppComponent extends React.Component {
     render() {
+        const { currentUser } = this.props;
+
         return (
             <Router history={history}>
                 <div>
@@ -20,9 +25,21 @@ export class App extends React.Component {
                 <div>
                     <Route path="/login" component={PageLogin} />
                     <Route path="/sign-up" component={PageSignUp} />
-                    <Route path="/home" exact component={PageHome} />
+                    {(currentUser && <Route path="/home" exact component={PageTimeLog} />) || (
+                        <Route path="/home" exact component={PageHome} />
+                    )}
                 </div>
             </Router>
         );
     }
 }
+
+const mapStateToProps = state => {
+    return {
+        currentUser: state.currentUser
+    };
+};
+
+const App = connect(mapStateToProps)(AppComponent);
+
+export { App };
