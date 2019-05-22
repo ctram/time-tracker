b6a6c31f8b48e7192054e86b037e59435a2c6df3
Chris Tram <chris@mytime.com>
2019-05-21 22:14:11 -0700

    stub pate for time log

---
+++ public/javascripts/pages/page-time-log.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Tracker } from '../components/tracker';
+
+export class PageTimeLog extends React.Component {
+    render() {
+        return (
+            <div className="d-flex justify-content-center">
+                <Tracker />
+            </div>
+        );
+    }
+}
