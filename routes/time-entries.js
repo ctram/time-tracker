73fb320b38e54a6ab0b0c59ebd19f4e984b43206
Chris Tram <ctram@protonmail.com>
2019-05-31 18:19:57 -0700

    stubs api route for time entry

---
+++ routes/time-entries.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+
+router.get('/running'(req, res, next) => {
+    // TODO: return only the running entry
+})
+
+module.exports = router;
