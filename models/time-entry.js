19999107a41f740385dc7e88111ba548664a5dff
Chris Tram <ctram@protonmail.com>
2019-05-31 18:17:44 -0700

    rename time-entry model

---
+++ models/time-entry.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const sequelize = require('../db/index');
+const Sequelize = require('Sequelize');
+const Model = Sequelize.Model;
+
+const User = require('./user');
+
+class TimeEntry extends Model { }
+
+TimeEntry.init(
+  {
+    // attributes
+    notes: {
+      type: Sequelize.STRING
+    },
+    startTime: {
+      type: Sequelize.DATE,
+      allowNull: false
+    },
+    endTime: {
+      type: Sequelize.DATE
+    }
+  },
+  {
+    sequelize
+    // options
+  }
+);
+
+TimeEntry.hasOne(User);
+
+module.exports = TimeEntry;
