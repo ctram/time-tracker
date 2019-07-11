b8fb5fca37ab1d80364fa6e6127bc00c3419efa1
Chris Tram <chris@mytime.com>
2019-07-10 22:00:32 -0700

    formatting

--- models/time-entry.js
+++ models/time-entry.js
@@ -4,20 +4,20 @@ const sequelize = require('../db/index');
 const Sequelize = require('Sequelize');
 
 const TimeEntry = sequelize.define('TimeEntry', {
-  // attributes
-  notes: {
-    type: Sequelize.STRING
-  },
-  startTime: {
-    type: Sequelize.DATE
-  },
-  endTime: {
-    type: Sequelize.DATE
-  },
-  userId: {
-    type: Sequelize.INTEGER,
-    allowNull: false
-  }
+    // attributes
+    notes: {
+        type: Sequelize.STRING
+    },
+    startTime: {
+        type: Sequelize.DATE
+    },
+    endTime: {
+        type: Sequelize.DATE
+    },
+    userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
 });
 
 module.exports = TimeEntry;
--- routes/time-entries.js
+++ routes/time-entries.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
-router.get('/running'(req, res, next) => {
-    // TODO: return only the running entry
-})
+router.get('/running', (req, res, next) => {
+    console.log('called from /running')
+});
 
 module.exports = router;
