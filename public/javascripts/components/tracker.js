957b543b8da4f2fa87fe4ddd5d81d4ae78f5b931
Chris Tram <chris@mytime.com>
2019-07-11 22:23:47 -0700

    stubs front end call to back when creating a time entry

--- public/javascripts/components/tracker.js
+++ public/javascripts/components/tracker.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { fetchPlus } from '../helpers/fetch-plus';
+
 
 export class TrackerComponent extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.trackerInput = React.createRef();
+
+        this.startTracker = this.startTracker.bind(this);
+    }
+
+    startTracker() {
+        const { currentUser } = this.props;
+
+        if (!this.trackerInput.current.value) {
+            return console.log('Tracker would start, but there is no title for the tracker, so not starting.')
+        }
+
+        console.log(this.trackerInput.current.value)
+
+        fetchPlus('http://localhost:3000/time-entries', {
+            method: 'POST',
+            body: JSON.stringify({ title: this.trackerInput.current.value, userId: currentUser.id })
+        })
+            .then(res => {
+
+            })
+            .catch(e => {
+                console.error(e);
+            })
+
+    }
+
     render() {
         return (
             <div className="tracker d-flex align-items-center w-50">
-                <input className="form-control form-control-lg mr-3" />
-                <i className="fas fa-play" onClick={this.props.startTracking} /> //
+                <input className="form-control form-control-lg mr-3" ref={this.trackerInput} />
+                <i className="fas fa-play" onClick={this.startTracker} />
             </div>
         );
     }
@@ -16,13 +48,14 @@ export class TrackerComponent extends React.Component {
 
 const mapStateToProps = state => {
     return {
-        isTrackerRunning: state.trackers.isTrackerRunning
+        isTrackerRunning: state.trackers.isTrackerRunning,
+        currentUser: state.users.currentUser
     };
 };
 
 const mapDispatchToProps = (dispatch, props) => {
     return {
-        startTracking: () => {}
+        // startTracking: () => {}
     };
 };
 
